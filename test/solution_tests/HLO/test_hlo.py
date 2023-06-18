ab31404e8c3413c015dcedb6faee81e410ea2a8b
sbx_user1051 <sbx_user1051@169.254.156.45>
2023-06-18 18:03:43 +0000

    Sun Jun 18 18:03:43 UTC 2023

--- test/solution_tests/HLO/test_hlo.py
+++ test/solution_tests/HLO/test_hlo.py
@@ -1,16 +1,8 @@
-from solutions.SUM import sum_solution
+from solutions.HLO import hello_solution
 import unittest
 
 class TestSum(unittest.TestCase):
-    def test_sum(self):
-        assert sum_solution.compute(1, 2) == 3
 
     def test_input_below_0(self):
-        with self.assertRaises(ValueError) as ex:
-            sum_solution.compute(101, 10)
-        self.assertTrue(ex.exception, "Input numbers should be between 0 and 100")
+        self.assertTrue(hello_solution.hello("joe"), "hello world")
 
-    def test_input_above_100(self):
-        with self.assertRaises(ValueError) as ex:
-            sum_solution.compute(-1, 10)
-        self.assertTrue(ex.exception, "Input numbers should be between 0 and 100")
