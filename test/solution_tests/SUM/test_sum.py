1242a0f42144d4c8b0baa285c28e46f2d40be91a
sbx_user1051 <sbx_user1051@169.254.156.45>
2023-06-18 17:58:25 +0000

    Sun Jun 18 17:58:25 UTC 2023

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -5,9 +5,15 @@ class TestSum(unittest.TestCase):
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
 
-    def test_validations(self):
+    def test_input_below_0(self):
         with self.assertRaises(ValueError) as ex:
-            sum_solution.compute(101, 200)
+            sum_solution.compute(101, 10)
         self.assertTrue(ex.exception, "Input numbers should be between 0 and 100")
+
+    def test_input_above_100(self):
+        with self.assertRaises(ValueError) as ex:
+            sum_solution.compute(-1, 10)
+        self.assertTrue(ex.exception, "Input numbers should be between 0 and 100")
+
 
 
