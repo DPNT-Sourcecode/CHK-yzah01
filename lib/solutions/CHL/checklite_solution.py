77fb71cdb8d1bb3ccf981c2733ec47ec6ec44286
sbx_user1051 <sbx_user1051@169.254.156.45>
2023-06-18 18:25:08 +0000

    Sun Jun 18 18:25:08 UTC 2023

--- lib/solutions/CHL/checklite_solution.py
+++ lib/solutions/CHL/checklite_solution.py
@@ -2,10 +2,24 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
 def checklite(skus): # AAABBCD
 
     skus = list(skus)
-    sku_qtys = 
+    sku_qtys = {}
+    for item in skus:
+        sku_qtys[item] = sku_qtys.get(item, 0) + 1
 
-    ntedError()
+    prices = {
+        "3A": 130,
+        "2B": 45,
+        "A" : 50,
+        "B" : 30,
+        "C" : 20,
+        "D" : 15
+    }
+
+    offer_under_A = int(sku_qtys.get("A")/3)
+
+
 
