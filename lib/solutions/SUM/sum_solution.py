be3fc53846cce36ed6a702b54ae142a87051c1b6
sbx_user1051 <sbx_user1051@169.254.156.45>
2023-06-18 17:57:16 +0000

    Sun Jun 18 17:57:16 UTC 2023

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,4 +1,8 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
+
 def compute(x, y):
+    if (x < 0 or x > 100) or (y < 0 or y > 100):
+        raise ValueError("Input numbers should be between 0 and 100")
     return x + y
+
 
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,10 +1,13 @@
 from solutions.SUM import sum_solution
+import unittest
 
-
-class TestSum():
+class TestSum(unittest.TestCase):
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
 
     def test_validations(self):
-        assert sum_solution.compute(101, 200) is None
+        with self.assertRaises(ValueError) as ex:
+            sum_solution.compute(101, 200)
+        self.assertTrue(ex.exception, "Input numbers should be between 0 and 100")
+
 
