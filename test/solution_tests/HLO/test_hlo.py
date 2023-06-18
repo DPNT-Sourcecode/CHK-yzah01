483a75f78b15d7fd2bc0153ea5f8fdaa59f4a8d7
sbx_user1051 <sbx_user1051@169.254.156.45>
2023-06-18 18:04:19 +0000

    Sun Jun 18 18:04:19 UTC 2023

--- test/solution_tests/HLO/test_hlo.py
+++ test/solution_tests/HLO/test_hlo.py
@@ -4,5 +4,6 @@ import unittest
 class TestSum(unittest.TestCase):
 
     def test_input_below_0(self):
-        self.assertTrue(hello_solution.hello("joe"), "hello world")
+        self.assertTrue(hello_solution.hello("joe"), "Hello, World!")
+
 
