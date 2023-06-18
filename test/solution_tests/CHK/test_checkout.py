2e1da0ffc16453604b61bfb73e32ff6359362685
sbx_user1051 <sbx_user1051@169.254.156.45>
2023-06-18 18:38:00 +0000

    Sun Jun 18 18:38:00 UTC 2023

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -8,11 +8,13 @@ class TestSum(unittest.TestCase):
         self.assertTrue(checkout_solution.checkout("AAAABBB"), 255)
         self.assertTrue(checkout_solution.checkout("A"), 50)
         self.assertTrue(checkout_solution.checkout("B"), 30)
+        self.assertTrue(checkout_solution.checkout("ABAB"), 145)
 
     def test_checkout_with_invalid_input(self):
         self.assertTrue(checkout_solution.checkout("ABCDE"), -1)
         self.assertTrue(checkout_solution.checkout(""), -1)
         self.assertTrue(checkout_solution.checkout(None), -1)
+
 
 
 
